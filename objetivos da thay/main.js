3a0a16578c6534ce83247e5a3e1ced0480dc958b
thay barbosa <128057978+obathay@users.noreply.github.com>
2024-05-06 08:31:45 -0300

    Add files via upload

--- objetivos da thay/main.js
+++ objetivos da thay/main.js
@@ -1,7 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date ("2034-01-05T00:00:00");
+const tempoObjetivo1 = new Date ("2031-01-22T00:00:00");
+const tempoObjetivo2 = new Date ("2034-01-22T00:00:00");
+const tempoObjetivo3 = new Date ("2037-01-22T00:00:00");
+const tempoObjetivo4 = new Date ("2034-01-22T00:00:00");
+const tempoObjetivo5 = new Date ("2039-01-22T00:00:00");
+const tempoObjetivo6 = new Date ("2034-01-22T00:00:00");
+const tempoObjetivo7 = new Date ("2030-01-05T00:00:00");
+const tempoObjetivo8 = new Date ("2024-12-08T10:00:00");
 const tempoAtual = new Date;
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
 
@@ -14,9 +21,34 @@ for(let i=0;i <botoes.length;i++){
         }
         //Remove a classe "ativo" de todos os botões.
         botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo")
+        textos[i].classList.add("ativo");
         //Adiciona a classe ativo só no botão clicado.
     }
 }
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
+contadores[4].textContent = calculaTempo(tempoObjetivo5);
+contadores[5].textContent = calculaTempo(tempoObjetivo6);
+contadores[6].textContent = calculaTempo(tempoObjetivo7);
+contadores[7].textContent = calculaTempo(tempoObjetivo8);
+
+
+
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual= new Date();
+    let tempoFinal= tempoObjetivo-tempoAtual;
+    let segundos= Math.floor(tempoFinal/1000);
+    let minutos= Math.floor(segundos/60);
+    let horas= Math.floor(minutos/60);
+    let dias= Math.floor(horas/24);
+    segundos %= 60;
+    minutos  %= 60;
+    horas  %= 24;
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}
+
